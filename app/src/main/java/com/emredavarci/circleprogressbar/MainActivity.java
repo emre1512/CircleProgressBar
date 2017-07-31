1abab7e55a135658ae44ab716bda4fbae34e1049
M. Emre Davarcı <emre151291@gmail.com>
2017-08-01 01:19:12 +0300

    First commit

---
+++ app/src/main/java/com/emredavarci/circleprogressbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.emredavarci.circleprogressbar;
+
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 500); // see this max value coming back here, we animale towards that value
+        animation.setDuration (5000); //in milliseconds
+        animation.setInterpolator (new DecelerateInterpolator());
+        animation.start ();
+
+
+    }
+}
